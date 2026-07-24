b2c212ebf288cecda46a0d070aa037284907d205
kevinalvarel <muhammadkevinalvarel@gmail.com>
2026-07-24 11:14:29 +0700

    fix: E2E test dan bug navigate - login hanya redirect ke home jika berhasil

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,7 +5,7 @@
  *   1. Harus menampilkan halaman login dengan benar (form, input email, input password, tombol login)
  *   2. Harus menampilkan alert ketika email dan password kosong
  *   3. Harus menampilkan alert ketika login gagal (email/password salah)
- *   4. Harus berhasil login dan redirect ke halaman utama
+ *   4. Harus berhasil login dan redirect ke halaman utama dengan tombol logout muncul
  */
 
 describe('Login spec', () => {
@@ -20,20 +20,21 @@ describe('Login spec', () => {
   });
 
   it('should display alert when email is empty', () => {
-    cy.get('#btn-login-submit').click();
     cy.on('window:alert', (str) => {
       expect(str).to.be.a('string');
     });
+    cy.get('#btn-login-submit').click();
   });
 
   it('should display alert when login fails with wrong credentials', () => {
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('email or password is wrong');
+    });
     cy.get('#input-email').type('wronguser@example.com');
     cy.get('#input-password').type('wrongpassword');
     cy.get('#btn-login-submit').click();
 
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal('email or password is wrong');
-    });
+    cy.url().should('include', '/login');
   });
 
   it('should login successfully and redirect to homepage', () => {
